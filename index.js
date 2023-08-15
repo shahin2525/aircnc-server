5e3b0eb4ada00b4b6b6237089ef0c7949e5afaa4
shahin2525 <shahin451999@gmail.com>
2023-08-15 08:56:40 -0700

    stripe route added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const morgan = require("morgan");
 const port = process.env.PORT || 5000;
-
+// middleware
 const corsOptions = {
   origin: "*",
   credentials: true,
@@ -13,7 +15,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json());
-
+// app.usemorgan("dev");
+app.use(morgan("dev"));
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ax6qyiu.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,7 +28,28 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+// verify jwt
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized Access" });
+  }
+  const token = authorization.split(" ")[1];
 
+  // console.log(token);
+  // verifyToken
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "Unauthorized Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,12 +57,23 @@ async function run() {
 
     const usersCollection = client.db("aircncDb").collection("users");
     const roomsCollection = client.db("aircncDb").collection("rooms");
+    const bookingsCollection = client.db("aircncDb").collection("bookings");
+    // generate jsonwebtoken
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      // console.log(email);
+      // console.log(token);
+      res.send({ token });
+    });
 
     app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
 
       const user = req.body;
-      console.log(user);
+      // console.log(user);
       const query = { email: email };
 
       const options = { upsert: true };
@@ -46,7 +81,7 @@ async function run() {
         $set: user,
       };
       const result = await usersCollection.updateOne(query, updateDoc, options);
-      console.log(result);
+      // console.log(result);
       res.send(result);
     });
 
@@ -71,18 +106,80 @@ async function run() {
       const result = await roomsCollection.find().toArray();
       res.send(result);
     });
-    // get single single room
+    // delete room
+    app.delete("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // get  single room
     app.get("/room/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await roomsCollection.findOne(query);
       res.send(result);
     });
-    // Save a bookin in database
+    // get all rooms for single host
+    app.get("/rooms/:email", verifyJwt, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+
+      console.log(decodedEmail);
+      const email = req.params.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "Forbidden Access" });
+      }
+      const query = { "host.email": email };
+      const result = await roomsCollection.find(query).toArray();
+      res.send(result);
+    });
+    //  get booking for guest
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { "guest.email": email };
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+    //  get booking for host
+    app.get("/bookings/host", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { host: email };
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Save a booking in database
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
 
-      const result = await roomsCollection.insertOne(booking);
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+    // update room booking status
+    app.patch("/rooms/status/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: { booked: status },
+      };
+      const update = await roomsCollection.updateOne(query, updateDoc);
+      res.send(update);
+    });
+    // delete booking
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(query);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
